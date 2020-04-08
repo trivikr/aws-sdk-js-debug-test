f4875a5e61f4640ca3f389e2d23bfa638967d254
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-04-08 14:45:32 -0700

    Add debug statements during client creation

--- packages/client/src/S3Client.js
+++ packages/client/src/S3Client.js
@@ -1,6 +1,12 @@
 const { S3Client: client } = require("@aws-sdk/client-s3");
+const debug = require("debug")("aws:sdk:js:s3");
 
-const S3Client = (params) => new client(params);
+const S3Client = (params) => {
+  debug("creating client with params", params);
+  const clientObj = new client(params);
+  debug("client created");
+  return clientObj;
+};
 
 module.exports = {
   S3Client,
